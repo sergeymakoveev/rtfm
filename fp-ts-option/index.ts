3d86fae9622596b28dc66e76f3c79ae627264d8a
Sergey Makoveev <sergey@makoveev.info>
2021-01-07 14:02:48 +0300

    fp-ts-option: change example

--- fp-ts-option/index.ts
+++ fp-ts-option/index.ts
@@ -1,15 +1,17 @@
-// import { sequenceT } from 'fp-ts/lib/Apply';
-// import { constant, pipe } from 'fp-ts/lib/function';
-// import { fold, fromNullable, getOrElse, map, option, toNullable } from 'fp-ts/lib/Option';
-import { getNullableValue } from '../helpers';
+import { sequenceT } from 'fp-ts/lib/Apply';
+import { constant, identity, pipe } from 'fp-ts/lib/function';
+import { fold, fromNullable, getOrElse, map, option, some, toNullable } from 'fp-ts/lib/Option';
+
+export const getRandomBoolean = () => Math.random() > 0.5;
+export const getNullableValue = <T>(value: T): T | null => (getRandomBoolean() ? value : null);
 
 const bid = getNullableValue(10);
 const ask = getNullableValue(7);
 // const profit = getNullableValue(1);
 
-const delta = bid !== null && ask !== null ? Math.abs(bid - ask) : null;
-const side = bid !== null && ask !== null && bid - ask > 0 ? 'sell' : 'buy';
-console.log({ bid, ask, delta, side });
+// const delta = bid !== null && ask !== null ? Math.abs(bid - ask) : null;
+// const side = bid !== null && ask !== null && bid - ask > 0 ? 'sell' : 'buy';
+// console.log({ bid, ask, delta, side });
 
 // const getDelta = (bid: number, ask: number) => Math.abs(bid - ask);
 // const getSide = (bid: number, ask: number) => (bid - ask > 0 ? 'sell' : 'buy');
@@ -38,6 +40,27 @@ console.log({ bid, ask, delta, side });
 // 	delta: deltaOption.map(delta => `${delta}`).getOrElse('-'),
 // 	side: sideOption.getOrElse('sell'),
 // });
+
+const arrayNumber = [1, 2]; // Array<number>
+console.log(
+	arrayNumber.map(value => value + 1),
+	arrayNumber.filter(value => Boolean(value % 2)),
+	arrayNumber.some(value => value % 2),
+	arrayNumber.every(value => value % 2),
+	arrayNumber.reduce((acc, value) => ({ ...acc, [value]: value }), {}),
+);
+
+const optionNumber = fromNullable(getNullableValue(1)); // Option<number>
+console.log(
+	optionNumber.map(value => value + 1),
+	optionNumber.filter(value => Boolean(value % 2)),
+	optionNumber.exists(value => Boolean(value % 2)),
+	optionNumber.reduce({}, (acc, value) => ({ ...acc, [value]: value })),
+	optionNumber.fold(0, value => value),
+	optionNumber.fold(0, identity),
+	optionNumber.getOrElse(0),
+	optionNumber.alt(some(2)),
+);
 
 // const bidOption = fromNullable(bid);
 // const askOption = fromNullable(ask);
