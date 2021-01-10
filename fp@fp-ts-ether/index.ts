0ab8c6a56a0578004c7c38cf973c3b1bbf027791
Sergey Makoveev <sergey@makoveev.info>
2021-01-10 12:39:41 +0300

    fp-ts-ether: change example

--- fp@fp-ts-ether/index.ts
+++ fp@fp-ts-ether/index.ts
@@ -11,12 +11,12 @@ const parseJson = (input: string): Either<Error, unknown> => {
 		const numbers = JSON.parse(input);
 		return E.right(numbers);
 	} catch (error) {
-		return E.left(error);
+		return E.left(new Error('Enter array of number, e.g. [1,2,3]'));
 	}
 };
 
 const checkArray = (value: unknown): Either<Error, Array<unknown>> =>
-	Array.isArray(value) ? E.right(value) : E.left(new Error('Enter array of number only, e.g. [1,2,3].'));
+	Array.isArray(value) ? E.right(value) : E.left(new Error(`Isn't enter valid array: ${value}`));
 
 const getAverage = (arr: Array<number>): number => {
 	const total = arr.length;
@@ -26,9 +26,9 @@ const getAverage = (arr: Array<number>): number => {
 
 const checkNumber = (value: number): Either<Error, number> =>
 	isNaN(value)
-		? E.left(new Error('Enter numbers only.'))
+		? E.left(new Error(`Enter numbers only: ${value}`))
 		: value === Infinity
-		? E.left(new Error('Enter some numbers.'))
+		? E.left(new Error(`Enter empty array: ${value}`))
 		: E.right(value);
 
 pipe(
