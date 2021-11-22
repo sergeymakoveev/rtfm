c1fe1f4f3050f3fbec86ac7e1422bbffb4aea2af
Sergey Makoveev <sergey@makoveev.info>
2021-11-23 01:30:05 +0300

    add test@memo

---
+++ test@memo/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env ts-node
+
+function sum(a: number, b: number) {
+	console.log('start');
+	return a + b;
+}
+
+function memo(fn) {
+	let memoized: Array<{ memoizedArgs: Array<unknown>; memoizedResult: Array<unknown> }> = [];
+	return function (...args) {
+		const memoizedIndex = memoized.findIndex(({ memoizedArgs }) =>
+			args.map((arg, index) => arg === memoizedArgs[index]).reduce((acc, value) => acc && value, true),
+		);
+		if (memoizedIndex !== -1) {
+			return memoized[memoizedIndex].memoizedResult;
+		}
+
+		const result = fn(...args);
+		memoized = [...memoized, { memoizedArgs: args, memoizedResult: result }];
+		return result;
+	};
+}
+
+const memoizedSum = memo(sum);
+
+console.log(sum(1, 2));
+console.log(memoizedSum(1, 2));
+console.log(memoizedSum(1, 2));
+console.log(memoizedSum(5, 2));
+console.log(memoizedSum(5, 2));
