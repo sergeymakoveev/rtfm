5cc330bc1b7e6ec6825246809e22ecdcd0854ae8
Sergey Makoveev <sergmakoveev@yandex-team.ru>
2025-07-02 10:41:36 +0300

    update type-printer

--- monaco.json-editor/src/type-printer.ts
+++ monaco.json-editor/src/type-printer.ts
@@ -13,56 +13,118 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename);
 
 function extractTypeSignature(filename: string, aliasNames: string[]): string {
-	console.log('## ', { filename, aliasNames });
-
 	const program: ts.Program = ts.createProgram([filename], {
 		emitDeclarationOnly: true,
 	});
 	const sourceFile: ts.SourceFile | undefined = program.getSourceFile(filename);
+
 	if (!sourceFile) {
 		throw new Error(`Source file: '${filename}' not found`);
 	}
+
 	const typeChecker: ts.TypeChecker = program.getTypeChecker();
 
 	return aliasNames
 		.map(aliasName => {
 			// Get the declaration node you're looking for by it's type name.
 			// This condition can be adjusted to your needs
+
+			console.log('## Processed', { aliasName });
+
+			const isSkipped = ['JSONArray', 'JSONObject', 'Indent', 'Project', 'TankerData'].includes(aliasName);
+
 			const statement: ts.Statement | undefined = sourceFile.statements.find(
 				s => ts.isTypeAliasDeclaration(s) && s.name.text === aliasName,
 			);
+
 			if (!statement) {
 				throw new Error(`Type: '${aliasName}' not found in file: '${filename}'`);
 			}
-			const type: ts.Type = typeChecker.getTypeAtLocation(statement);
-			console.log('## ', type.getProperties());
 
-			const props = type.getProperties();
-			const fields: string[] = [];
-			// Iterate over the `ts.Symbol`s representing Property Nodes of `ts.Type`
-			for (const prop of props) {
-				const name: string = prop.getName();
-				const propType: ts.Type = typeChecker.getTypeOfSymbolAtLocation(prop, statement);
-				const propTypeName: string = typeChecker.typeToString(
-					propType,
-					undefined,
-					ts.TypeFormatFlags.NoTruncation,
-				);
-				fields.push(`${name}: ${propTypeName};`);
+			const type: ts.Type = typeChecker.getTypeAtLocation(statement);
+			let stringifiedType: string;
+
+			const getTypeSignature = (type: ts.Type) => {
+				const fields: string[] = [];
+				const props = type.getProperties();
+
+				// Iterate over the `ts.Symbol`s representing Property Nodes of `ts.Type`
+				for (const prop of props) {
+					const name: string = prop.getName();
+					const propType: ts.Type = typeChecker.getTypeOfSymbolAtLocation(prop, statement);
+					const propTypeName: string = typeChecker.typeToString(
+						propType,
+						undefined,
+						ts.TypeFormatFlags.NoTruncation,
+					);
+
+					fields.push(`${name}: ${propTypeName};`);
+				}
+
+				return `{\n  ${fields.join('\n  ')}\n}`;
+			};
+
+			// const stringIndexType = type.getStringIndexType();
+
+			if (isSkipped) {
+				console.log('## Skipped, set to "any"', { aliasName, flags: type.getFlags() });
+				// type.aliasSymbol;
+				stringifiedType = 'any';
+			} else if (type.isUnion()) {
+				stringifiedType = type.types
+					.map(
+						/*
+                    // eslint-disable-next-line no-nested-ternary
+                    t => t.isStringLiteral()
+                        ? `'${t.value}'`
+                        : t.isLiteral()
+                            ? t.value
+                            : getTypeSignature(t)
+                    */
+						typeInUnion => {
+							const propTypeName: string = typeChecker.typeToString(
+								typeInUnion,
+								undefined,
+								ts.TypeFormatFlags.NoTruncation,
+							);
+
+							return propTypeName;
+						},
+					)
+					.join(' | ');
+				// } else if (type.getFlags() === TypeFlags.Object && stringIndexType) {
+				//     console.log('## ', { aliasName });
+				//     // type.aliasSymbol;
+				//     stringifiedType = 'any';
+			} else {
+				stringifiedType = getTypeSignature(type);
 			}
-			return `type ${aliasName} = {\n  ${fields.join('\n  ')}\n}`;
+
+			return `type ${aliasName} = ${stringifiedType};`;
 		})
 		.join('\n\n');
 }
 
 const typeSignature = extractTypeSignature(`${__dirname}/type-printer.config.ts`, TYPES);
 
-// write to file or console log
-// console.log(typeSignature);
+const code = [
+	'/* eslint-disable max-len */',
+	'/* eslint-disable quotes */',
+	'/* eslint-disable @typescript-eslint/no-explicit-any */',
+	'/* eslint-disable @typescript-eslint/no-unused-vars */',
+	'// eslint-disable-next-line @typescript-eslint/ban-ts-comment',
+	'// @ts-nocheck',
+	'',
+	typeSignature,
+	'',
+].join('\n');
 
 import('fs')
 	.then(fs => {
-		fs.writeFileSync(`${__dirname}/type-printer.results.ts`, typeSignature, {});
+		fs.writeFileSync(`${__dirname}/type-printer.results.ts`, code, {});
+
 		return fs;
 	})
-	.catch(() => {});
+	.catch(() => {
+		console.error('Error by write result');
+	});
--- ts.type-printer/type-printer.ts
+++ ts.type-printer/type-printer.ts
@@ -1,53 +1,130 @@
-import * as ts from 'typescript';
+#!/usr/bin/env tsx
+
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+import ts from 'typescript'; // get the name of the directory
 
 import { TYPES } from './type-printer.config';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// eslint-disable-next-line @typescript-eslint/naming-convention
+// const __dirname = path.dirname(__filename);
 
 function extractTypeSignature(filename: string, aliasNames: string[]): string {
 	const program: ts.Program = ts.createProgram([filename], {
 		emitDeclarationOnly: true,
 	});
 	const sourceFile: ts.SourceFile | undefined = program.getSourceFile(filename);
+
 	if (!sourceFile) {
 		throw new Error(`Source file: '${filename}' not found`);
 	}
+
 	const typeChecker: ts.TypeChecker = program.getTypeChecker();
 
 	return aliasNames
 		.map(aliasName => {
 			// Get the declaration node you're looking for by it's type name.
 			// This condition can be adjusted to your needs
+
+			console.log('## Processed', { aliasName });
+
+			const isSkipped = ['JSONArray', 'JSONObject', 'Indent', 'Project', 'TankerData'].includes(aliasName);
+
 			const statement: ts.Statement | undefined = sourceFile.statements.find(
 				s => ts.isTypeAliasDeclaration(s) && s.name.text === aliasName,
 			);
+
 			if (!statement) {
 				throw new Error(`Type: '${aliasName}' not found in file: '${filename}'`);
 			}
+
 			const type: ts.Type = typeChecker.getTypeAtLocation(statement);
-			const fields: string[] = [];
-			// Iterate over the `ts.Symbol`s representing Property Nodes of `ts.Type`
-			for (const prop of type.getProperties()) {
-				const name: string = prop.getName();
-				const propType: ts.Type = typeChecker.getTypeOfSymbolAtLocation(prop, statement);
-				const propTypeName: string = typeChecker.typeToString(
-					propType,
-					undefined,
-					ts.TypeFormatFlags.NoTruncation,
-				);
-				fields.push(`${name}: ${propTypeName};`);
+			let stringifiedType: string;
+
+			const getTypeSignature = (type: ts.Type) => {
+				const fields: string[] = [];
+				const props = type.getProperties();
+
+				// Iterate over the `ts.Symbol`s representing Property Nodes of `ts.Type`
+				for (const prop of props) {
+					const name: string = prop.getName();
+					const propType: ts.Type = typeChecker.getTypeOfSymbolAtLocation(prop, statement);
+					const propTypeName: string = typeChecker.typeToString(
+						propType,
+						undefined,
+						ts.TypeFormatFlags.NoTruncation,
+					);
+
+					fields.push(`${name}: ${propTypeName};`);
+				}
+
+				return `{\n  ${fields.join('\n  ')}\n}`;
+			};
+
+			// const stringIndexType = type.getStringIndexType();
+
+			if (isSkipped) {
+				console.log('## Skipped, set to "any"', { aliasName, flags: type.getFlags() });
+				// type.aliasSymbol;
+				stringifiedType = 'any';
+			} else if (type.isUnion()) {
+				stringifiedType = typeChecker.typeToString(type, undefined, ts.TypeFormatFlags.NoTruncation);
+
+				const _stringifiedType = type.types
+					.map(
+						/*
+                    // eslint-disable-next-line no-nested-ternary
+                    t => t.isStringLiteral()
+                        ? `'${t.value}'`
+                        : t.isLiteral()
+                            ? t.value
+                            : getTypeSignature(t)
+                    */
+						typeInUnion => {
+							const propTypeName = typeInUnion.isLiteral()
+								? typeChecker.typeToString(typeInUnion, undefined, ts.TypeFormatFlags.NoTruncation)
+								: getTypeSignature(typeInUnion);
+
+							return propTypeName;
+						},
+					)
+					.join(' | ');
+				// } else if (type.getFlags() === TypeFlags.Object && stringIndexType) {
+				//     console.log('## ', { aliasName });
+				//     // type.aliasSymbol;
+				//     stringifiedType = 'any';
+			} else {
+				stringifiedType = getTypeSignature(type);
 			}
-			return `type ${aliasName} = {\n  ${fields.join('\n  ')}\n}`;
+
+			return `type ${aliasName} = ${stringifiedType};`;
 		})
 		.join('\n\n');
 }
 
 const typeSignature = extractTypeSignature(`${__dirname}/type-printer.config.ts`, TYPES);
 
-// write to file or console log
-// console.log(typeSignature);
+const code = [
+	'/* eslint-disable max-len */',
+	'/* eslint-disable quotes */',
+	'/* eslint-disable @typescript-eslint/no-explicit-any */',
+	'/* eslint-disable @typescript-eslint/no-unused-vars */',
+	'// eslint-disable-next-line @typescript-eslint/ban-ts-comment',
+	'// @ts-nocheck',
+	'',
+	typeSignature,
+	'',
+].join('\n');
 
 import('fs')
 	.then(fs => {
-		fs.writeFileSync(`${__dirname}/type-printer.results.ts`, typeSignature, {});
+		fs.writeFileSync(`${__dirname}/type-printer.results.ts`, code, {});
+
 		return fs;
 	})
-	.catch(() => {});
+	.catch(() => {
+		console.error('Error by write result');
+	});
