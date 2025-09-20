baeae46939c868286c435b590c68db1dfa1c9f7d
Sergey Makoveev <sergey@makoveev.info>
2025-09-20 08:57:19 +0300

    update tasks

--- interview/task-tree-abstract.ts
+++ interview/task-tree-abstract.ts
@@ -87,3 +87,115 @@
 		reduceBfs(tree, reducer, []),
 	);
 }
+{
+	type Tree<Value> = {
+		value: Value;
+		children: Tree<Value>[];
+	};
+
+	const tree: Tree<number> = {
+		value: 1,
+		children: [
+			{
+				value: 11,
+				children: [
+					{
+						value: 111,
+						children: [],
+					},
+					{
+						value: 112,
+						children: [],
+					},
+					{
+						value: 113,
+						children: [],
+					},
+				],
+			},
+			{
+				value: 12,
+				children: [
+					{
+						value: 121,
+						children: [],
+					},
+					{
+						value: 122,
+						children: [],
+					},
+					{
+						value: 123,
+						children: [],
+					},
+				],
+			},
+			{
+				value: 13,
+				children: [
+					{
+						value: 131,
+						children: [],
+					},
+					{
+						value: 132,
+						children: [],
+					},
+					{
+						value: 133,
+						children: [],
+					},
+				],
+			},
+		],
+	};
+
+	/*
+	1
+	21 22 23
+	31 32
+	*/
+
+	const traversePreOrderDfs = <Value>(tree: Tree<Value>): Value[] => [
+		tree.value,
+		...tree.children.reduce((acc, tree) => [...acc, ...traversePreOrderDfs(tree)], [] as Value[]),
+	];
+	console.log('## traversePreOrderDfs', traversePreOrderDfs(tree));
+
+	const traversePostOrderDfs = <Value>(tree: Tree<Value>): Value[] => [
+		...tree.children.reduce((acc, tree) => [...acc, ...traversePostOrderDfs(tree)], [] as Value[]),
+		tree.value,
+	];
+	console.log('## traversePostOrderDfs', traversePostOrderDfs(tree));
+
+	const traverseBfs = <Value>(tree: Tree<Value>, isRoot = true): Value[] => [
+		...(isRoot ? [tree.value] : []),
+		...tree.children.map(tree => tree.value),
+		...tree.children.reduce((acc, tree) => [...acc, ...traverseBfs(tree, false)], [] as Value[]),
+	];
+	console.log('## traverseBfs', traverseBfs(tree));
+
+	const map = <Value, ValueMapped>(tree: Tree<Value>, fn: (value: Value) => ValueMapped): Tree<ValueMapped> => ({
+		value: fn(tree.value),
+		children: tree.children.map(tree => map(tree, fn)),
+	});
+	console.log(
+		'## map',
+		JSON.stringify(
+			map(tree, value => `*${value}`),
+			null,
+			4,
+		),
+	);
+
+	const reduce = <Value, ValueReduced>(
+		tree: Tree<Value>,
+		fn: (acc: ValueReduced, tree: Value) => ValueReduced,
+		acc: ValueReduced,
+	): ValueReduced => tree.children.reduce((acc, tree) => reduce(tree, fn, acc), fn(acc, tree.value));
+
+	console.log(
+		'## reduce',
+		reduce(tree, (acc, value) => `${acc}*${value}`, ''),
+	);
+}
--- interview/task-typescript.ts
+++ interview/task-typescript.ts
@@ -1,94 +1,120 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-/*
-Написать собственную реализацию типа Omit
-*/
 {
-	type MyOmit<T extends Object, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
-	type ExObj = { a: boolean; b: boolean; c: boolean };
-	type ExOmit = Omit<ExObj, 'b'>;
-	type ExMyOmit = MyOmit<ExObj, 'b'>;
-}
-
-/*
-Написать собственную реализацию типа Pick
-*/
-{
-	type MyPick<T extends Object, K extends keyof T> = { [P in K]: T[P] };
-	type ExObj = { a: boolean; b: boolean; c: boolean };
-	type ExPick = Pick<ExObj, 'b'>;
-	type ExMyPick = MyPick<ExObj, 'b'>;
-}
-
-/*
-2. Дженерики
-Задача:
-Создайте универсальную функцию getValue, которая принимает объект и ключ,
-и возвращает значение этого ключа из объекта.
-Добавьте проверку типов, чтобы ключ обязательно существовал в объекте.
-*/
-{
-	const getValue = <T extends Object = {}>(obj: T, key: keyof T) => obj[key];
-
-	console.log(getValue({ a: 1 }, 'a'));
-}
-
-/*
-3. Условные типы (Conditional Types)
-Задача:
-Напишите тип NonNullableProps<T>, который делает все свойства объекта T обязательными (удаляет null и undefined).
-*/
-{
-	type NonNullableProps<T extends Object = Object> = {
-		[K in keyof T]-?: T[K] extends null | undefined ? never : T[K];
+	type ExObj = {
+		a: boolean;
+		b: boolean;
+		c: null;
+		d?: number;
+		e: string | null;
+		f: undefined;
+		g?: undefined;
 	};
 
-	type NonNullableProps2<T extends Object = Object> = {
-		[K in keyof T]-?: NonNullable<T[K]>;
-	};
+	/* Написать собственную реализацию типа Omit */
+	{
+		type MyOmit<T extends Object, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
+		type ExObj = { a: boolean; b: boolean; c: boolean };
+		type ExOmit = Omit<ExObj, 'b'>;
+		type ExMyOmit = MyOmit<ExObj, 'b'>;
+	}
+	{
+		type MyOmit1<T extends Object, K extends keyof T> = { [F in Exclude<keyof T, K>]: T[F] };
+		type MyOmit2<T extends Object, K extends keyof T> = {
+			[F in keyof T as F extends K ? never : F]: F extends K ? never : T[F];
+		};
+		type MyOmit1Res = MyOmit1<ExObj, 'a'>;
+		type MyOmit2Res = MyOmit2<ExObj, 'a'>;
+	}
+	/* Написать собственную реализацию типа Pick */
+	{
+		type MyPick<T extends Object, K extends keyof T> = { [P in K]: T[P] };
+		type ExObj = { a: boolean; b: boolean; c: boolean };
+		type ExPick = Pick<ExObj, 'b'>;
+		type ExMyPick = MyPick<ExObj, 'b'>;
+	}
+	{
+		type MyPick<T extends Object, K extends keyof T> = { [F in K]: T[F] };
+		type MyPickRes = MyPick<ExObj, 'b'>;
+	}
 
-	type T = NonNullableProps<{ a?: string; b: number }>;
-	type T2 = NonNullableProps2<{ a?: string; b: number }>;
+	/**
+	 * 2. Дженерики
+	 * Задача:
+	 * Создайте универсальную функцию getValue, которая принимает объект и ключ,
+	 * и возвращает значение этого ключа из объекта.
+	 * Добавьте проверку типов, чтобы ключ обязательно существовал в объекте.
+	 **/
+	{
+		const getValue = <T extends Object = {}>(obj: T, key: keyof T) => obj[key];
 
-	type TT = T['a'];
-	type TT2 = T2['a'];
-}
-/*
-4. Утилитарные типы
-Задача:
-Реализуйте свой аналог встроенного типа Pick<T, K>.
-*/
-{
-	type Pick2<T extends Object, P extends keyof T> = {
-		[K in P]: T[P];
-	};
+		console.log(getValue({ a: 1 }, 'a'));
+	}
+	{
+		type GetValueFn<T extends Object, K extends keyof T> = (obj: T, key: K) => T[K];
+		const getValue = <T extends Object, K extends keyof T>(obj: T, key: K): T[K] => obj[key];
+	}
 
-	type P = Pick<{ a: 1; b: 2 }, 'a'>;
-	type P2 = Pick2<{ a: 1; b: 2 }, 'a'>;
-}
-/*
+	/**
+	 * 3. Условные типы (Conditional Types)
+	 * Задача:
+	 * Напишите тип NonNullableProps<T>, который делает все свойства объекта T обязательными (удаляет null и undefined).
+	 **/
+	{
+		type NonNullableProps<T extends Object = Object> = {
+			[K in keyof T]-?: Exclude<T[K], null | undefined>;
+		};
+
+		type NonNullableProps2<T extends Object = Object> = {
+			[K in keyof T]-?: NonNullable<T[K]>;
+		};
+
+		type T = NonNullableProps<ExObj>;
+		type T2 = NonNullableProps2<ExObj>;
+	}
+	{
+		type NonNullableProps<T extends Object> = { [K in keyof T]-?: Exclude<T[K], undefined | null> };
+		type NonNullableProps2<T extends Object> = {
+			[K in keyof T]-?: null extends T[K] ? never : undefined extends T[K] ? never : T[K];
+		};
+		type T = NonNullableProps<ExObj>;
+		type T2 = NonNullableProps2<ExObj>;
+	}
+
+	/**
+	 * OmitNullable<T>
+	 */
+
+	{
+		type OmitNullableProps<T extends Object> = {
+			[K in keyof T as null extends T[K] ? never : undefined extends T[K] ? never : K]: T[K];
+		};
+		type T = OmitNullableProps<ExObj>;
+	}
+
+	/*
 5. Перегрузка функций (Function Overloads)
 Задача:
 Напишите функцию formatInput, которая:
 Если на входе строка — возвращает её в верхнем регистре.
 Если на входе число — возвращает его строковое представление с добавлением px (например, 10 → "10px").
 */
-{
-	const formatInput = <T extends string | number>(inp: T): T extends string ? Uppercase<T> : `${T}px` =>
-		typeof inp === 'string' ? inp.toUpperCase() : `${inp}px`;
+	{
+		const formatInput = <T extends string | number>(inp: T): T extends string ? Uppercase<T> : `${T}px` =>
+			typeof inp === 'string' ? inp.toUpperCase() : `${inp}px`;
 
-	formatInput('hello'); // "HELLO"
-	formatInput(20); // "20px"
-}
-/*
+		formatInput('hello'); // "HELLO"
+		formatInput(20); // "20px"
+	}
+	/*
 7. Продвинутые типы: Mapped Types + keyof
 Задача:
 Создайте тип ReadonlyDeep<T>, который делает все свойства объекта (включая вложенные) доступными только для чтения.
 */
-{
-	type ReadonlyDeep<T extends Object = Object> = {
-		readonly [K in keyof T]: T[K] extends Object ? ReadonlyDeep<T[K]> : T[K];
-	};
+	{
+		type ReadonlyDeep<T extends Object = Object> = {
+			readonly [K in keyof T]: T[K] extends Object ? ReadonlyDeep<T[K]> : T[K];
+		};
 
-	type TT = ReadonlyDeep<{ a: number; b: { c: string } }>;
+		type TT = ReadonlyDeep<{ a: number; b: { c: string } }>;
+	}
 }
