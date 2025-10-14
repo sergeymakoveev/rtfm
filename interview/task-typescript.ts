3e7031ef735b8a439ca8c6f473e3f629e9b94640
Sergey Makoveev <sergey@makoveev.info>
2025-10-14 11:21:00 +0300

    task-typescript: use object instead Object

--- interview/task-typescript.ts
+++ interview/task-typescript.ts
@@ -3,38 +3,46 @@
 	type ExObj = {
 		a: boolean;
 		b: boolean;
-		c: null;
-		d?: number;
-		e: string | null;
-		f: undefined;
-		g?: undefined;
+		c?: number;
+		d: string | null;
+		e: string | undefined;
+		f: null;
+		g: undefined;
+		h?: undefined;
 	};
 
 	/* Написать собственную реализацию типа Omit */
 	{
-		type MyOmit<T extends Object, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
+		type MyOmit<T extends object, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
 		type ExObj = { a: boolean; b: boolean; c: boolean };
 		type ExOmit = Omit<ExObj, 'b'>;
 		type ExMyOmit = MyOmit<ExObj, 'b'>;
 	}
 	{
-		type MyOmit1<T extends Object, K extends keyof T> = { [F in Exclude<keyof T, K>]: T[F] };
-		type MyOmit2<T extends Object, K extends keyof T> = {
-			[F in keyof T as F extends K ? never : F]: F extends K ? never : T[F];
+		type MyOmit1<T extends object, K extends keyof T> = { [F in Exclude<keyof T, K>]: T[F] };
+		type MyOmit2<T extends object, K extends keyof T> = {
+			[F in keyof T as F extends K ? never : F]: T[F];
 		};
 		type MyOmit1Res = MyOmit1<ExObj, 'a'>;
 		type MyOmit2Res = MyOmit2<ExObj, 'a'>;
 	}
 	/* Написать собственную реализацию типа Pick */
 	{
-		type MyPick<T extends Object, K extends keyof T> = { [P in K]: T[P] };
+		type MyPick<T extends object, K extends keyof T> = { [P in K]: T[P] };
 		type ExObj = { a: boolean; b: boolean; c: boolean };
 		type ExPick = Pick<ExObj, 'b'>;
 		type ExMyPick = MyPick<ExObj, 'b'>;
 	}
 	{
-		type MyPick<T extends Object, K extends keyof T> = { [F in K]: T[F] };
+		type MyPick<T extends object, K extends keyof T> = { [F in K]: T[F] };
 		type MyPickRes = MyPick<ExObj, 'b'>;
+	}
+	{
+		type MyPick<T extends object, U extends keyof T> = {
+			[K in keyof T as K extends U ? K : never]: T[K];
+		};
+		type T = MyPick<ExObj, 'c'>;
+		type TT = MyPick<number, 'c'>;
 	}
 
 	/**
@@ -45,13 +53,13 @@
 	 * Добавьте проверку типов, чтобы ключ обязательно существовал в объекте.
 	 **/
 	{
-		const getValue = <T extends Object = {}>(obj: T, key: keyof T) => obj[key];
+		const getValue = <T extends object = {}>(obj: T, key: keyof T) => obj[key];
 
 		console.log(getValue({ a: 1 }, 'a'));
 	}
 	{
-		type GetValueFn<T extends Object, K extends keyof T> = (obj: T, key: K) => T[K];
-		const getValue = <T extends Object, K extends keyof T>(obj: T, key: K): T[K] => obj[key];
+		type GetValueFn<T extends object, K extends keyof T> = (obj: T, key: K) => T[K];
+		const getValue = <T extends object, K extends keyof T>(obj: T, key: K): T[K] => obj[key];
 	}
 
 	/**
@@ -60,37 +68,82 @@
 	 * Напишите тип NonNullableProps<T>, который делает все свойства объекта T обязательными (удаляет null и undefined).
 	 **/
 	{
-		type NonNullableProps<T extends Object = Object> = {
-			[K in keyof T]-?: Exclude<T[K], null | undefined>;
+		type NonNullableProps<T extends object = object> = {
+			[K in keyof T as Exclude<T[K], null | undefined> extends never ? never : K]-?: Exclude<
+				T[K],
+				null | undefined
+			>;
 		};
 
-		type NonNullableProps2<T extends Object = Object> = {
-			[K in keyof T]-?: NonNullable<T[K]>;
+		type NonNullableProps2<T extends object = object> = {
+			[K in keyof T as NonNullable<T[K]> extends never ? never : K]-?: NonNullable<T[K]>;
 		};
 
 		type T = NonNullableProps<ExObj>;
 		type T2 = NonNullableProps2<ExObj>;
 	}
 	{
-		type NonNullableProps<T extends Object> = { [K in keyof T]-?: Exclude<T[K], undefined | null> };
-		type NonNullableProps2<T extends Object> = {
-			[K in keyof T]-?: null extends T[K] ? never : undefined extends T[K] ? never : T[K];
+		type NonNullableProps<T extends object> = {
+			[K in keyof T as Exclude<T[K], undefined | null> extends never ? never : K]-?: Exclude<
+				T[K],
+				undefined | null
+			>;
 		};
+		type MyExclude<T, U> = T extends U ? never : T;
+		type NonNullableProps2<T extends object> = {
+			[K in keyof T as T[K] extends null | undefined ? never : K]-?: MyExclude<T[K], null | undefined>;
+		};
+		type TT = 'a' extends 'a' ? true : false;
+		type TT1 = 'a' | 'b' extends 'a' ? true : false;
+		type TT2 = 'a' extends 'a' | 'b' ? true : false;
+		type TT3 = 'a' | undefined extends undefined ? true : false;
+		type TT4 = undefined extends 'a' | undefined ? true : false;
+
 		type T = NonNullableProps<ExObj>;
 		type T2 = NonNullableProps2<ExObj>;
+	}
+	{
+		type NonNullableProps<T extends object> = {
+			[K in keyof T as Exclude<T[K], null | undefined> extends never ? never : K]-?: Exclude<
+				T[K],
+				null | undefined
+			>;
+		};
+		type T = NonNullableProps<ExObj>;
 	}
 
 	/**
-	 * OmitNullable<T>
+	 * Реализовать Exclude<'a'|'b'|'c', 'a'|'c'> = 'b'
 	 */
-
 	{
-		type OmitNullableProps<T extends Object> = {
+		type MyExclude<T, U> = T extends U ? never : T;
+		type ExObjKeys = keyof ExObj;
+		type T = MyExclude<ExObjKeys, 'a' | 'b'>;
+		type T2 = MyExclude<string | null, null>;
+		type T3 = MyExclude<null, string | null>;
+		type T4 = MyExclude<string | null | undefined, undefined | null>;
+	}
+	{
+		type MyExclude<T, U> = T extends U ? never : T;
+		type T = Exclude<'a' | 'b' | 'c', 'a' | 'c'>;
+	}
+
+	/**
+	 * 4. Удаление всех nullable полей из типа объекта
+	 * OmitNullable<T>
+	 **/
+	{
+		type OmitNullableProps<T extends object> = {
 			[K in keyof T as null extends T[K] ? never : undefined extends T[K] ? never : K]: T[K];
 		};
 		type T = OmitNullableProps<ExObj>;
 	}
-
+	{
+		type OmitNullable<T extends object> = {
+			[K in keyof T as T[K] extends NonNullable<T[K]> ? K : never]: T[K];
+		};
+		type T = OmitNullable<ExObj>;
+	}
 	/*
 5. Перегрузка функций (Function Overloads)
 Задача:
@@ -99,8 +152,12 @@
 Если на входе число — возвращает его строковое представление с добавлением px (например, 10 → "10px").
 */
 	{
-		const formatInput = <T extends string | number>(inp: T): T extends string ? Uppercase<T> : `${T}px` =>
-			typeof inp === 'string' ? inp.toUpperCase() : `${inp}px`;
+		function formatInput(inp: string): Uppercase<string>;
+		function formatInput(inp: number): `${number}px`;
+
+		function formatInput(inp: string | number): Uppercase<string> | `${number}px` {
+			return typeof inp === 'string' ? (inp.toUpperCase() as Uppercase<string>) : `${inp}px`;
+		}
 
 		formatInput('hello'); // "HELLO"
 		formatInput(20); // "20px"
@@ -111,10 +168,18 @@
 Создайте тип ReadonlyDeep<T>, который делает все свойства объекта (включая вложенные) доступными только для чтения.
 */
 	{
-		type ReadonlyDeep<T extends Object = Object> = {
-			readonly [K in keyof T]: T[K] extends Object ? ReadonlyDeep<T[K]> : T[K];
+		type ReadonlyDeep<T extends object = object> = {
+			readonly [K in keyof T]: T[K] extends object ? ReadonlyDeep<T[K]> : T[K];
 		};
 
 		type TT = ReadonlyDeep<{ a: number; b: { c: string } }>;
+	}
+
+	{
+		type ReadonlyDeep<T extends object> = {
+			readonly [K in keyof T]: T[K] extends object ? ReadonlyDeep<T[K]> : T[K];
+		};
+
+		type T = ReadonlyDeep<ExObj & { aa: ExObj }>;
 	}
 }
